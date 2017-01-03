4d1991685a01658d57726cd234099abc8327351f
JohnathanInkley <jcniboc@gmail.com>
2017-01-03 14:49:07 +0000

    Changed variables to package private and removed unnecessary return statement

--- src/com/simpleEditor/TextEditor.java
+++ src/com/simpleEditor/TextEditor.java
@@ -1,16 +1,13 @@
 package com.simpleEditor;
 
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 import javax.swing.*;
-import javax.swing.text.*;
 
 public class TextEditor extends JFrame {
-    public JTextArea editorText = new JTextArea(40,80);
-    public String currentFile = "Untitled document";
-    public boolean modifiedFlag =  false;
-    public JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+    JTextArea editorText = new JTextArea(40,80);
+    String currentFile = "Untitled document";
+    boolean modifiedFlag =  false;
+    JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
 
     public TextEditor() {
         setFontSize(12);
@@ -19,24 +16,20 @@ public class TextEditor extends JFrame {
         setTitle(currentFile);
         pack();
         setVisible(true);
-        return;
     }
 
     private void attachComponents() {
         add(fileChooser);
         add(editorText);
-        return;
     }
 
     private void setFontSize(int fontSize) {
         editorText.setFont(new Font("Monospace", Font.PLAIN, fontSize));
-        return;
     }
 
     private void addScrollFeature() {
         JScrollPane scrollFeature = new JScrollPane(editorText);
         add(editorText, BorderLayout.CENTER);
-        return;
     }
 
     private void addMenuBar() {
@@ -44,7 +37,6 @@ public class TextEditor extends JFrame {
         addFileDropdown(menuBar);
         addEditDropdown(menuBar);
         setJMenuBar(menuBar);
-        return;
     }
 
     private void addFileDropdown(JMenuBar menuBar) {
@@ -53,7 +45,6 @@ public class TextEditor extends JFrame {
         addOpenFileOptionToMenu(fileDropdown);
         addNewFileOptionToMenu(fileDropdown);
         menuBar.add(fileDropdown);
-        return;
     }
 
     private void addEditDropdown(JMenuBar menuBar) {
@@ -62,47 +53,39 @@ public class TextEditor extends JFrame {
         addCopyTextOptionToMenu(editDropdown);
         addPasteTextOptionToMenu(editDropdown);
         menuBar.add(editDropdown);
-        return;
     }
 
     private void addSaveFileOptionToMenu(JMenu dropdown) {
         JMenuItem saveFileOption = new JMenuItem(new SaveFileOption(this));
         dropdown.add(saveFileOption);
-        return;
     }
 
     private void addOpenFileOptionToMenu(JMenu dropdown) {
         JMenuItem openFileOption = new JMenuItem(new OpenFileOption(this));
         dropdown.add(openFileOption);
-        return;
     }
 
     private void addNewFileOptionToMenu(JMenu dropdown) {
         JMenuItem newFileOption = new JMenuItem(new NewFileOption(this));
         dropdown.add(newFileOption);
-        return;
     }
 
     private void addCutTextOptionToMenu(JMenu dropdown) {
         JMenuItem cutTextOption = new JMenuItem(new CutTextOption(this));
         dropdown.add(cutTextOption);
-        return;
     }
 
     private void addCopyTextOptionToMenu(JMenu dropdown) {
         JMenuItem copyTextOption = new JMenuItem(new CopyTextOption(this));
         dropdown.add(copyTextOption);
-        return;
     }
 
     private void addPasteTextOptionToMenu(JMenu dropdown) {
         JMenuItem pasteTextOption = new JMenuItem(new PasteTextOption(this));
         dropdown.add(pasteTextOption);
-        return;
     }
 
     public static void main(String[] args) {
         new TextEditor();
-        return;
     }
 }
